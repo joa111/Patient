bbdde3f84f4877173166f3ca8e0031361f455be1
G. Joachim Dkhar <gjdkhar@gmail.com>
2025-09-06 17:46:17 +0000

    Try fixing this error: `Runtime Error: FirebaseError: Firebase: Error (a

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,15 +3,16 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration is now read from environment variables
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyB7gobp_RyD05hnVmFBdtX-pcjVRZrBUJ4",
+  authDomain: "order-63780.firebaseapp.com",
+  projectId: "order-63780",
+  storageBucket: "order-63780.appspot.com",
+  messagingSenderId: "57020655857",
+  appId: "1:57020655857:web:5aa6af025ca168e63ee150"
 };
+
 
 // Initialize Firebase
 // A check is added to ensure that firebase is only initialized once.
