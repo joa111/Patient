09c6d7057299a23b4731900fb0cc8d0ebb5a07cf
G. Joachim Dkhar <gjdkhar@gmail.com>
2025-09-08 19:13:34 +0000

    no bruh, What i mean is they will stick at the bottom of the page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -169,7 +169,7 @@ function ProfilePageContent() {
         </div>
       </CardHeader>
 
-      <CardContent className="p-4 md:p-6">
+      <CardContent className="p-4 md:p-6 md:pb-16">
         <PatientTabs patient={patient} />
       </CardContent>
     </Card>
@@ -237,11 +237,11 @@ export default function ProfilePage() {
 function PatientTabs({ patient }: { patient: Patient }) {
   return (
     <ShadTabs defaultValue="dashboard" className="w-full">
-      <TabsList className="flex flex-col h-auto sm:flex-row sm:h-10 bg-primary/10">
-        <TabsTrigger value="dashboard"><LayoutDashboard className="mr-2 h-4 w-4" />Dashboard</TabsTrigger>
-        <TabsTrigger value="overview"><User className="mr-2 h-4 w-4" />Overview</TabsTrigger>
-        <TabsTrigger value="find-nurse"><Search className="mr-2 h-4 w-4" />New Request</TabsTrigger>
-        <TabsTrigger value="history"><Calendar className="mr-2 h-4 w-4" />History</TabsTrigger>
+      <TabsList className="md:flex md:flex-row md:h-10 md:bg-primary/10 fixed bottom-0 left-0 right-0 z-50 flex h-16 justify-around bg-background shadow-[0_-1px_3px_rgba(0,0,0,0.1)] md:relative md:justify-start md:shadow-none md:rounded-md">
+        <TabsTrigger value="dashboard" className="flex-col px-2 h-full md:flex-row md:w-auto md:h-auto"><LayoutDashboard className="mr-0 md:mr-2 h-5 w-5" />Dashboard</TabsTrigger>
+        <TabsTrigger value="overview" className="flex-col px-2 h-full md:flex-row md:w-auto md:h-auto"><User className="mr-0 md:mr-2 h-5 w-5" />Overview</TabsTrigger>
+        <TabsTrigger value="find-nurse" className="flex-col px-2 h-full md:flex-row md:w-auto md:h-auto"><Search className="mr-0 md:mr-2 h-5 w-5" />New Request</TabsTrigger>
+        <TabsTrigger value="history" className="flex-col px-2 h-full md:flex-row md:w-auto md:h-auto"><Calendar className="mr-0 md:mr-2 h-5 w-5" />History</TabsTrigger>
       </TabsList>
       <TabsContent value="dashboard" className="mt-6">
         <DashboardTab patientId={patient.id} />
